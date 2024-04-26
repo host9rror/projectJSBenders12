34661d1efe59677a8aa7383b1d99e5f247c4eb2a
zero <sanyafullgit@gmail.com>
2024-04-26 16:46:46 +0300

    Update faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,17 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   const triggers = document.querySelectorAll('.ac-trigger, .accordion-btn');
+
+//   triggers.forEach(trigger => {
+//     trigger.addEventListener('click', function() {
+//       const content = this.parentElement.nextElementSibling; // Find the next sibling of the parent element
+//       content.classList.toggle('is-hidden');
+//     });
+//   });
+// });
+
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener("DOMContentLoaded", function() {
-  const accordion = new Accordion('.faq-heading'); // Correcting the class to target
+document.addEventListener('DOMContentLoaded', function() {
+  new Accordion('.faq-list');
 });
