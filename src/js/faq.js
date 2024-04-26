764cad535701617c6bd84667b56e1230c03808ff
zero <sanyafullgit@gmail.com>
2024-04-26 15:05:09 +0300

    js accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,6 +1,17 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+document.addEventListener("DOMContentLoaded", function() {
+  const faqHeadings = document.querySelectorAll(".faq-heading");
 
-new Accordion('.faq-accordion-container', {
-  showMultiple: true,
+  faqHeadings.forEach(function(heading) {
+    heading.addEventListener("click", function() {
+      const content = this.nextElementSibling;
+
+      // Toggle the 'is-hidden' class
+      content.classList.toggle("is-hidden");
+
+      // Toggle the rotation of the arrow icon
+      const arrowIcon = this.querySelector(".accordion-btn svg");
+      arrowIcon.classList.toggle("rotate-180");
+    });
+  });
 });
+
