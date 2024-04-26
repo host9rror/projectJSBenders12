4fd112ce8307e6a758509383fafccb71276f42dd
zero <sanyafullgit@gmail.com>
2024-04-26 17:23:23 +0300

    acordion script

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,17 +1,11 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const triggers = document.querySelectorAll('.ac-trigger, .accordion-btn');
-
-//   triggers.forEach(trigger => {
-//     trigger.addEventListener('click', function() {
-//       const content = this.parentElement.nextElementSibling; // Find the next sibling of the parent element
-//       content.classList.toggle('is-hidden');
-//     });
-//   });
-// });
-
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
 document.addEventListener('DOMContentLoaded', function() {
-  new Accordion('.faq-list');
+  const triggers = document.querySelectorAll('.ac-trigger, .accordion-btn');
+
+  triggers.forEach(trigger => {
+    trigger.addEventListener('click', function() {
+      const content = this.parentElement.nextElementSibling; // Find the next sibling of the parent element
+      content.classList.toggle('is-hidden');
+    });
+  });
 });
+
