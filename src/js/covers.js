f63ec01308bff4332214549a49c52fcd6483aa96
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-04-26 21:33:44 +0300

    added images and animation
    I will work more on the smother animation

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,17 +1,17 @@
+const coversProjects = document.querySelector('.covers-projects')
 const covers =
     [{
-        mobile:'/src/img/covers/project-1.png', desktop:'/src/img/covers/project-1@2x.png',description:'project 1'},
-        {mobile:'/src/img/covers/project-2.png', desktop:'/src/img/covers/project-2@2x.png',description:'project 2'},
-        {mobile:'/src/img/covers/project-3.png', desktop:'/src/img/covers/project-3@2x.png',description:'project 3'},
-      { mobile:'/src/img/covers/project-4.png', desktop:'/src/img/covers/project-4@2x.png',description:'project 4'},
-        {mobile:'/src/img/covers/project-5.png', desktop:'/src/img/covers/project-5@2x.png',description:'project 5'},
-      { mobile: '/src/img/covers/project-6.png', desktop:'/src/img/covers/project-6@2x.png',description:'project 6'},
-        {mobile:'/src/img/covers/project-7.png', desktop:'/src/img/covers/project-7@2x.png',description:'project 7'},
-        {mobile:'/src/img/covers/project-8.png', desktop:'/src/img/covers/project-8@2x.png',description:'project 8'},
-        {mobile:'/src/img/covers/project-9.png', desktop:'/src/img/covers/project-9@2x.png',description:'project 9'},
-        {mobile:'/src/img/covers/project-10.png', desktop:'/src/img/covers/project-10@2x.png',description:'project 10'},
-        {mobile:'/src/img/covers/project-11.png', desktop:'/src/img/covers/project-11@2x.png',description:'project 11'},
-        {mobile:'/src/img/covers/project-12.png', desktop:'/src/img/covers/project-12@2x.png',description:'project 12'}]
+        mobile:'../img/covers/project-1.png', desktop:'../img/covers/project-1@2x.png',description:'project 1'},
+        {mobile:'../img/covers/project-2.png', desktop:'../img/covers/project-2@2x.png',description:'project 2'},
+        {mobile:'../img/covers/project-3.png', desktop:'../img/covers/project-3@2x.png',description:'project 3'},
+      { mobile:'../img/covers/project-4.png', desktop:'../img/covers/project-4@2x.png',description:'project 4'},
+      { mobile: '../img/covers/project-6.png', desktop:'../img/covers/project-6@2x.png',description:'project 6'},
+        {mobile:'../img/covers/project-7.png', desktop:'../img/covers/project-7@2x.png',description:'project 7'},
+        {mobile:'../img/covers/project-8.png', desktop:'../img/covers/project-8@2x.png',description:'project 8'},
+        {mobile:'../img/covers/project-9.png', desktop:'../img/covers/project-9@2x.png',description:'project 9'},
+        {mobile:'../img/covers/project-10.png', desktop:'../img/covers/project-10@2x.png',description:'project 10'},
+        {mobile:'../img/covers/project-11.png', desktop:'../img/covers/project-11@2x.png',description:'project 11'},
+        ]
 console.log(covers);
 function coversItems(arr) {
     return arr.map(({mobile, desktop, description}) =>
