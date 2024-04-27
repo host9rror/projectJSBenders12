257798520fc00aab3ea0d7dff5509e82e5c6f32d
AnastasiiaTitarenko <151573636+AnastasiiaTitarenko@users.noreply.github.com>
2024-04-28 01:23:43 +0300

    додано в JS
      додано код перевірка настання кінця карток для того щоб задізеблити кнопку

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -3,30 +3,41 @@ import axios from 'axios';
 
 const Base_URL = 'https://portfolio-js.b.goit.study/api';
 
-// document.addEventListener('DOMContentLoaded', async () => {}
     const BtnNext = document.querySelector('.btn-arrow-right');
     const BtnPrev = document.querySelector('.btn-arrow-left');
 
-    async function getReviews() {
+async function getReviews() {
     try {
         const response = await axios.get(`${Base_URL}/reviews`);
-    
         const reviews = response.data;
         renderReviews(reviews);
 
         let swiper = new Swiper('.reviews-swiper-container.swiper-container', {
         direction: 'horizontal',
-        keyboard: {enabled: true, onlyInViewport: true},
+            keyboard: { enabled: true, onlyInViewport: true },
+            touch: true,
             navigation: {
                 nextEl: '.btn-arrow-right',
                 prevEl: '.btn-arrow-left'},
- });
+        });
+        
         BtnNext.addEventListener('click', function () {
             swiper.slideNext();
         });
         BtnPrev.addEventListener('click', function () {
             swiper.slidePrev();
         });
+        swiper.on('reachEnd', function () {
+            BtnNext.disabled = true;
+        });
+        swiper.on('reachBeginning', function () {
+            BtnPrev.disabled = true;
+        });
+        swiper.on('fromEdge', function () {
+            BtnNext.disabled = false;
+            BtnPrev.disabled = false;
+        });
+        
     } catch (error) {
         alert('An error occurred while getting reviews');
         document.querySelector('.swiper-wrapper').innerHTML = '<p>Not found</p>';
@@ -47,4 +58,5 @@ function renderReviews(reviews) {
 }
 
 getReviews();
+
 
