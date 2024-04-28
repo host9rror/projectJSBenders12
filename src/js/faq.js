452c64452ee4027dfc1839f95c2ba1d208713959
zero <sanyafullgit@gmail.com>
2024-04-28 18:13:06 +0300

    accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,44 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const triggers = document.querySelectorAll('.ac-trigger, .accordion-btn');
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-  triggers.forEach(trigger => {
-    trigger.addEventListener('click', function() {
-      const content = this.parentElement.nextElementSibling; 
-      content.classList.toggle('is-hidden');
 
-      const svg = this.parentElement.querySelector('svg'); 
-      
-      if (content.classList.contains('is-hidden')) {
-        svg.classList.remove('rotate-180');
-      } else {
-        svg.classList.add('rotate-180');
-      }
-    });
-  });
+new Accordion('.accordion-container', {
+  duration: 400,
+  showMultiple: true,
+  onOpen: function(currentElement) {
+    console.log(currentElement);
+  }
 });
-
-
-// import Accordion from 'accordion-js';
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const accordion = new Accordion('.faq-accordion-container', {
-//     open: false,
-//     collapsible: true,
-//     multiple: false,
-//     event: 'click',
-//    selector: {
-//       trigger: '.ac-trigger',
-//       content: '.faq-content'
-//     },
-//     callback: {
-//       open: function(event) {
-//         const svg = event.target.closest('.faq-heading').querySelector('svg');
-//         svg.classList.add('rotate-180');
-//       },
-//       close: function(event) {
-//         const svg = event.target.closest('.faq-heading').querySelector('svg');
-//         svg.classList.remove('rotate-180');
-//       }
-//     }
-//   });
-// });
