7009815bf7cbefc200e4cdd08e627ecaabc737ff
zero <sanyafullgit@gmail.com>
2024-04-28 18:48:00 +0300

    little changes

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,11 +1,29 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
 
+// document.addEventListener("DOMContentLoaded", function() {
+//   const accordion = new Accordion('.faq-list', {
+//     duration: 300,
+//     beforeToggle: function(toggleElement, panel) {
+//       const svg = toggleElement.querySelector('svg');
+//       if (panel.classList.contains('is-visible')) {
+//         svg.classList.remove('rotate-180');
+//       } else {
+//         svg.classList.add('rotate-180');
+//       }
+//     }
+//   });
+// });
 
-new Accordion('.accordion-container', {
-  duration: 400,
-  showMultiple: true,
-  onOpen: function(currentElement) {
-    console.log(currentElement);
-  }
+const accordionHeaders = document.querySelectorAll('.ac-header');
+
+accordionHeaders.forEach(header => {
+  header.addEventListener('click', () => {
+    const panel = header.nextElementSibling.querySelector('.faq-content'); 
+    const svg = header.querySelector('svg'); 
+
+    panel.classList.toggle('is-hidden');
+
+    svg.classList.toggle('rotate-180');
+  });
 });
