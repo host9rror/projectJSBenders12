ef0aa70ece48e50e1e3f514435a38944ce127d43
Bohdan Matsko <bm01271970@gmail.com>
2024-04-30 21:44:41 +0200

    first/style/mobile

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,10 +3,39 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.project', {
-    speed: 400,
-    spaceBetween: 100,
-    direction: 'horizontal',
-  });
+  loop: true,
+  direction: 'horizontal',
+  allowSlideNext: true,
+  setWrapperSize: true,
+  modules: [Navigation, Keyboard, Mousewheel],
+  simulateTouch: true,
+  grabCursor: true,
+  spaceBetween: 0,
+  speed: 1000,
+  navigation: {
+    nextEl: '.about-swiper-button-next',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    375: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    invert: true,
+  },
+});
 
   const nextButton = document.querySelector('.arrow-right');
   const prevButton = document.querySelector('.arrow-left');
