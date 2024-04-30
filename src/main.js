db24c90f23688a1457ea4d6e68d7d79cce9ade8c
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-04-30 17:18:23 +0300

    modal`s paddings

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import './js/faq.js';
 import './js/header.js';
 import './js/hero.js';
 import './js/projects.js';
-import './js/reviews.js';
+// import './js/reviews.js';
 import './js/work-together.js';
